650437de68f4a03a47fbe164e9d98a5b5e29ff03
patrickRobotics <pirungu9@gmail.com>
2020-04-14 14:40:05 +0300

    cleanup and refactors

--- src/estimator.js
+++ src/estimator.js
@@ -15,8 +15,8 @@ const computeFactor = (periodType, timeToElapse) => {
   return Math.floor(days / 3);
 };
 
-const computeImpact = (data) => {
-  const currentlyInfected = data.reportedCases * 10;
+const getImpact = (data, isSevere) => {
+  const currentlyInfected = isSevere ? data.reportedCases * 50 : data.reportedCases * 10;
   const days = normalizeDays(data.periodType, data.timeToElapse);
   const factor = computeFactor(data.periodType, data.timeToElapse);
   const infectionsByRequestedTime = currentlyInfected * (2 ** factor);
@@ -47,61 +47,13 @@ const computeImpact = (data) => {
   return impact;
 };
 
-const computeSevereImpact = (data) => {
-  const currentlyInfected = data.reportedCases * 50;
-  const days = normalizeDays(data.periodType, data.timeToElapse);
-  const factor = computeFactor(data.periodType, data.timeToElapse);
-  const infectionsByRequestedTime = currentlyInfected * (2 ** factor);
-  const severeCasesByRequestedTime = Math.floor(infectionsByRequestedTime * 0.15);
-  const hospitalBedsByRequestedTime = Math.trunc(
-    data.totalHospitalBeds * 0.35 - severeCasesByRequestedTime
-  );
-  const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
-  const casesForVentilatorsByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
-  const dollarsInFlight = Math.trunc(
-    (
-      data.region.avgDailyIncomePopulation
-      * data.region.avgDailyIncomeInUSD
-      * infectionsByRequestedTime
-    ) / days
-  );
-
-  const severeImpact = {
-    currentlyInfected,
-    infectionsByRequestedTime,
-    severeCasesByRequestedTime,
-    hospitalBedsByRequestedTime,
-    casesForICUByRequestedTime,
-    casesForVentilatorsByRequestedTime,
-    dollarsInFlight
-  };
-
-  return severeImpact;
-};
-
 const covid19ImpactEstimator = (data) => {
-  const impactData = computeImpact(data);
-  const severeImpactData = computeSevereImpact(data);
+  const impact = getImpact(data);
+  const severeImpact = getImpact(data, true);
   return {
     data,
-    impact: {
-      currentlyInfected: impactData.currentlyInfected,
-      infectionsByRequestedTime: impactData.infectionsByRequestedTime,
-      severeCasesByRequestedTime: impactData.severeCasesByRequestedTime,
-      hospitalBedsByRequestedTime: impactData.hospitalBedsByRequestedTime,
-      casesForICUByRequestedTime: impactData.casesForICUByRequestedTime,
-      casesForVentilatorsByRequestedTime: impactData.casesForVentilatorsByRequestedTime,
-      dollarsInFlight: impactData.dollarsInFlight
-    },
-    severeImpact: {
-      currentlyInfected: severeImpactData.currentlyInfected,
-      infectionsByRequestedTime: severeImpactData.infectionsByRequestedTime,
-      severeCasesByRequestedTime: severeImpactData.severeCasesByRequestedTime,
-      hospitalBedsByRequestedTime: severeImpactData.hospitalBedsByRequestedTime,
-      casesForICUByRequestedTime: severeImpactData.casesForICUByRequestedTime,
-      casesForVentilatorsByRequestedTime: severeImpactData.casesForVentilatorsByRequestedTime,
-      dollarsInFlight: severeImpactData.dollarsInFlight
-    }
+    impact,
+    severeImpact
   };
 };
 
--- src/test/Estimator.test.js
+++ src/test/Estimator.test.js
@@ -1,7 +1,7 @@
 import covid19ImpactEstimator from '../estimator';
 
 describe('Covid-19 Estimator', () => {
-  const testData = {
+  const timeInDaysTestData = {
     data: {
       region: {
         name: 'Africa',
@@ -36,10 +36,109 @@ describe('Covid-19 Estimator', () => {
       }
     }
   };
-  it('Should pass challenge 1', () => {
-    const result = covid19ImpactEstimator(testData.data);
-    expect(result.data).toEqual(expect.objectContaining(testData.data));
-    expect(testData.estimate.impact).toEqual(expect.objectContaining(result.impact));
-    expect(testData.estimate.severeImpact).toEqual(expect.objectContaining(result.severeImpact));
+
+  const timeInWeeksTestData = {
+    data: {
+      region: {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 3,
+        avgDailyIncomePopulation: 0.62
+      },
+      periodType: 'weeks',
+      timeToElapse: 5,
+      reportedCases: 2125,
+      population: 6602148,
+      totalHospitalBeds: 134415
+    },
+    estimate: {
+      impact: {
+        currentlyInfected: 21250,
+        infectionsByRequestedTime: 43520000,
+        severeCasesByRequestedTime: 6528000,
+        hospitalBedsByRequestedTime: -6480954,
+        casesForICUByRequestedTime: 2176000,
+        casesForVentilatorsByRequestedTime: 870400,
+        dollarsInFlight: 2312777
+      },
+      severeImpact: {
+        currentlyInfected: 106250,
+        infectionsByRequestedTime: 217600000,
+        severeCasesByRequestedTime: 32640000,
+        hospitalBedsByRequestedTime: -32592954,
+        casesForICUByRequestedTime: 10880000,
+        casesForVentilatorsByRequestedTime: 4352000,
+        dollarsInFlight: 11563885
+      }
+    }
+  };
+
+  const timeInMonthsTestData = {
+    data: {
+      region: {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 2,
+        avgDailyIncomePopulation: 0.7
+      },
+      periodType: 'months',
+      timeToElapse: 3,
+      reportedCases: 2015,
+      population: 8323658,
+      totalHospitalBeds: 109404
+    },
+    estimate: {
+      impact: {
+        currentlyInfected: 20150,
+        infectionsByRequestedTime: 21635897753600,
+        severeCasesByRequestedTime: 3245384663040,
+        hospitalBedsByRequestedTime: -3245384624748,
+        casesForICUByRequestedTime: 1081794887680,
+        casesForVentilatorsByRequestedTime: 432717955072,
+        dollarsInFlight: 336558409500
+      },
+      severeImpact: {
+        currentlyInfected: 100750,
+        infectionsByRequestedTime: 108179488768000,
+        severeCasesByRequestedTime: 16226923315200,
+        hospitalBedsByRequestedTime: -16226923276908,
+        casesForICUByRequestedTime: 5408974438400,
+        casesForVentilatorsByRequestedTime: 2163589775360,
+        dollarsInFlight: 1682792047502
+      }
+    }
+  };
+
+  it('Test for time in days', () => {
+    const result = covid19ImpactEstimator(timeInDaysTestData.data);
+    expect(result.data).toEqual(expect.objectContaining(timeInDaysTestData.data));
+    expect(expect.objectContaining(result.impact)).toEqual(
+      timeInDaysTestData.estimate.impact
+    );
+    expect(result.severeImpact).toEqual(
+      expect.objectContaining(timeInDaysTestData.estimate.severeImpact)
+    );
+  });
+
+  it('Test for time in weeks', () => {
+    const result = covid19ImpactEstimator(timeInWeeksTestData.data);
+    expect(result.data).toEqual(expect.objectContaining(timeInWeeksTestData.data));
+    expect(result.impact).toEqual(
+      expect.objectContaining(timeInWeeksTestData.estimate.impact)
+    );
+    expect(result.severeImpact).toEqual(
+      expect.objectContaining(timeInWeeksTestData.estimate.severeImpact)
+    );
+  });
+
+  it('Test for time in months', () => {
+    const result = covid19ImpactEstimator(timeInMonthsTestData.data);
+    expect(result.data).toEqual(expect.objectContaining(timeInMonthsTestData.data));
+    expect(result.impact).toEqual(
+      expect.objectContaining(timeInMonthsTestData.estimate.impact)
+    );
+    expect(result.severeImpact).toEqual(
+      expect.objectContaining(timeInMonthsTestData.estimate.severeImpact)
+    );
   });
 });
